1e7fcf4f558e130219396224983facd5ab454308
Rakesh Nandakumar <Rakeshnandakumar2005@gmail.com>
2024-12-15 14:50:35 +0000

    local changes

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -144,61 +144,7 @@ namespace TestApp.Controllers
     // Redirect to the Index page of the product
     return RedirectToAction("Index", new { productId = imageDTO.ProductId });
 }
-
-
-        // GET: Image/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var image = await _context.Images.FindAsync(id);
-            if (image == null)
-            {
-                return NotFound();
-            }
-            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Id", image.ProductId);
-            return View(image);
-        }
-
-        // POST: Image/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Url,IsMain,ProductId")] Image image)
-        {
-            if (id != image.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(image);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ImageExists(image.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Id", image.ProductId);
-            return View(image);
-        }
-
+   
         // GET: Image/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
